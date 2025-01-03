95fa4c1e9ff7e81d5b1201a290fa2241a3cc1cdf
shashikant021 <shashikantsahu487@gmail.com>
2025-01-03 11:34:07 +0530

    add button on page not exist

--- Frontend/src/pages/not-found/notfound.jsx
+++ Frontend/src/pages/not-found/notfound.jsx
@@ -1,10 +1,14 @@
+import { Button } from '@/components/ui/button'
 import React from 'react'
 
 function NotFound() {
   return (
-    <div>
-      Page doesn't exist
+    <>
+    <div className='text-center pt-10'>
+      Page doesn't exist {' '}
+      <Button className='bg-slate-600'> <a href="/shop/home"> Home Page</a></Button>
     </div>
+    </>
   )
 }
 
